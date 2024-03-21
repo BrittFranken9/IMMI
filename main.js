ae347d9f221b94ed3cbf7bff4d87e7120f252ce3
BrittFranken <160475036+BrittFranken@users.noreply.github.com>
2024-03-21 11:02:17 +0100

    foto met knop

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let ws = new WebSocket('ws://10.150.197.78:1880/websocket');
 
-setupCounter(document.querySelector('#counter'))
+ws.onopen = function() {
+    console.log('Connected to server');
+}
+
+ws.onmessage = function(event) {
+  console.log('Message from server ', event.data);
+
+  if (typeof event.data === 'object') {
+    // Assuming the message is a blob
+    var reader = new FileReader();
+    reader.onload = function() {
+        var base64data = reader.result;
+        // Use the base64 data as image src
+        var img = document.querySelector('#image');
+        img.src = base64data;
+    };
+    reader.readAsDataURL(event.data);
+}
+
+ 
+}
